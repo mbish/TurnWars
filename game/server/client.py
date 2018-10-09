f451109b43f068e7f99c6292de668b00fea02d2f
Mark Bishop <mbishop@duosecurity.com>
2018-10-09 07:27:53 -0400

    Added autobahn and missing __init__ file

--- game/server/client.py
+++ game/server/client.py
@@ -3,7 +3,7 @@ import hashlib
 
 class Client:
     def __init__(self, socket):
-        self.id_string = hashlib.sha256(socket.ip.encode('utf-8')).hexdigest()[0:32] #uuid.uuid4().hex
+        self.id_string = hashlib.sha256(socket.ip.encode('utf-8')).hexdigest()[0:32] #uuid.uuid4().he
         self.socket = socket
         self.spectating = False
 
